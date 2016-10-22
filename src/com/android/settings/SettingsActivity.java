2d4d3579237fadf18b229379cad83e53a7e0639e
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2016-10-22 03:56:06 +0200

    Clean omnigears inclusion

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -144,8 +144,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import org.omnirom.omnigears.OmniGearsSettings;
-
 public class SettingsActivity extends SettingsDrawerActivity
         implements PreferenceManager.OnPreferenceTreeClickListener,
         PreferenceFragment.OnPreferenceStartFragmentCallback,
@@ -388,8 +386,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             MasterClear.class.getName(),
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
-            AutomaticStorageManagerSettings.class.getName(),
-            OmniGearsSettings.class.getName()
+            AutomaticStorageManagerSettings.class.getName()
     };
 
 
@@ -595,7 +592,6 @@ public class SettingsActivity extends SettingsDrawerActivity
 
         mIsShowingDashboard = className.equals(Settings.class.getName())
                 || className.equals(Settings.WirelessSettings.class.getName())
-                || className.equals(Settings.OmniGearsSettings.class.getName())
                 || className.equals(Settings.DeviceSettings.class.getName())
                 || className.equals(Settings.PersonalSettings.class.getName())
                 || className.equals(Settings.WirelessSettings.class.getName());
